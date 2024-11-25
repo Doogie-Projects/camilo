f355db6ccb444bbcbe6aa55eb4303d381d61cee1
ALFAROSO <alberth.sotelo@gmail.com>
2024-11-24 17:20:14 -0800

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecordsComponent } from './records/records.component';
+import { CollaboratorsComponent } from './collaborators/collaborators.component';
+import { CollaboratorDetailComponent } from './collaborator-detail/collaborator-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'records', component: RecordsComponent },
+  { path: 'collaborators', component: CollaboratorsComponent},
+  { path: 'collaborators/:collaboratorId', component: CollaboratorDetailComponent},
+  {path: '**', redirectTo: '/records', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
