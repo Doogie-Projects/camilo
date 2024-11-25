39ece4419a05045bf115042aa4ce0dd0f6ddbe06
ALFAROSO <alberth.sotelo@gmail.com>
2024-11-24 16:52:58 -0800

    new component for collaborators screen -colaboradores-

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecordsComponent } from './records/records.component';
+import { CollaboratorsComponent } from './collaborators/collaborators.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecordsComponent
+    RecordsComponent,
+    CollaboratorsComponent
   ],
   imports: [
     BrowserModule,
