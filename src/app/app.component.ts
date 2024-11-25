e0bae23516fa5b54b18ef8dd9d18dc1ca1cfcf27
ALFAROSO <alberth.sotelo@gmail.com>
2024-11-24 16:49:10 -0800

    addtailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FE-Camilo';
+  
+  ngOnInit() {
+    const menuButton = document.getElementById('menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuButton?.addEventListener('click', () => {
+      mobileMenu?.classList.toggle('hidden');
+    });
+  }
 }
