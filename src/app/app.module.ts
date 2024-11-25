133694c29f42cdeea64ffc0baed6ab092944344f
ALFAROSO <alberth.sotelo@gmail.com>
2024-11-24 16:51:50 -0800

    new component for records screen -registros-

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecordsComponent } from './records/records.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordsComponent
   ],
   imports: [
     BrowserModule,
