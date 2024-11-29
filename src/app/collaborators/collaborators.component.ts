01d6cc8bddb8c8b57dbc78774a3707b8db7b5b20
ALFAROSO <alberth.sotelo@gmail.com>
2024-11-29 14:10:42 -0800

    new collaborator using API

--- src/app/collaborators/collaborators.component.ts
+++ src/app/collaborators/collaborators.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-collaborators',
@@ -8,7 +9,9 @@ import { Router } from '@angular/router';
 })
 export class CollaboratorsComponent {
 
-  constructor(private router: Router) { }
+  data: any [] = [];
+
+  constructor(private router: Router, private apiService: ApiService) { }
 
   editCollaborator(collaboratorId: string) {
     this.router.navigate(['/collaborators', collaboratorId]);
@@ -22,5 +25,43 @@ export class CollaboratorsComponent {
   closeModal() {
     const modal = document.getElementById('modal');
     modal?.classList.add('hidden');
+  }
+
+  saveCollaborator() {
+    const email = (document.getElementById('new-email') as HTMLInputElement).value;
+    const name = (document.getElementById('new-name') as HTMLInputElement).value;
+    const lastName = (document.getElementById('new-lastname') as HTMLInputElement).value;
+    const password = (document.getElementById('new-password') as HTMLInputElement).value;
+    const employeeID = (document.getElementById('new-id') as HTMLInputElement).value;
+    const phone = (document.getElementById('new-phone') as HTMLInputElement).value;
+    const ssid = (document.getElementById('new-ssid') as HTMLInputElement).value;
+
+    const collaborator = {
+      email,
+      name,
+      lastName,
+      password,
+      employeeID,
+      phone,
+      ssid
+    };
+
+    this.apiService.createCollab(collaborator).subscribe(response => {
+      console.log('Collaborator updated:', response);
+      this.closeModal();
+    }, error => {
+      console.error('Error updating collaborator:', error);
+    });
+  }
+
+  ngOnInit() : void{
+    this.getAllColab()
+  }
+
+  getAllColab() {
+    this.apiService.getAllColab().subscribe((data: any) => {
+      this.data = data;
+      console.log(this.data);
+    });
   }
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private url = 'https://aconocutgf.execute-api.us-east-1.amazonaws.com/v1/'
+
+  constructor(private http: HttpClient) { }
+
+  public getAllColab(): Observable<any> {
+    const headers = new HttpHeaders().set('x-api-key', 'MN70HmszY51RXTqUpXnRz3812ZLfhxtE8N0LPPg4');
+    return this.http.get<any>(`${this.url}get-all`, { headers });
+  }
+
+  public createCollab(collaborator: any): Observable<any> {
+    const headers = new HttpHeaders().set('x-api-key', 'MN70HmszY51RXTqUpXnRz3812ZLfhxtE8N0LPPg4');
+    return this.http.put<any>(`${this.url}create`, collaborator, { headers });
+  }
 }
