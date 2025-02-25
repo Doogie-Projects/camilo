05357ab4ad9fb934c5f125ba3971f7f619580723
ALFAROSO <alberth.sotelo@gmail.com>
2025-02-25 11:59:51 -0800

    Add rout on app-routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecordsComponent } from './records/records.component';
 import { CollaboratorsComponent } from './collaborators/collaborators.component';
 import { CollaboratorDetailComponent } from './collaborator-detail/collaborator-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'records', component: RecordsComponent },
   { path: 'collaborators', component: CollaboratorsComponent},
   { path: 'collaborators/:collaboratorId', component: CollaboratorDetailComponent},
