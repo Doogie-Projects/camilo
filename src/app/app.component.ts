e9a9d643c0e305476f55e874f9d44613288cf3a6
ALFAROSO <alberth.sotelo@gmail.com>
2025-03-07 11:52:40 -0800

    Implement code on app.comp.ts to hide buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,16 @@ import { AuthService } from './auth.service';
 })
 export class AppComponent {
   title = 'FE-Camilo';
+  showNavItems = true;
 
-  constructor(private authService: AuthService) {}
-  
+  constructor(private authService: AuthService, private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showNavItems = event.url !== '/login';
+      }
+    });
+  }
+
   ngOnInit() {
     const menuButton = document.getElementById('menu-button');
     const mobileMenu = document.getElementById('mobile-menu');
