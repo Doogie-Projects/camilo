90e6bfe919cdd8d1fc6a261d31222acb20e8ddb2
ALFAROSO <alberth.sotelo@gmail.com>
2025-03-07 11:36:26 -0800

    protect rutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { RecordsComponent } from './records/records.component';
 import { CollaboratorsComponent } from './collaborators/collaborators.component';
 import { CollaboratorDetailComponent } from './collaborator-detail/collaborator-detail.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'records', component: RecordsComponent },
-  { path: 'collaborators', component: CollaboratorsComponent},
-  { path: 'collaborators/:collaboratorId', component: CollaboratorDetailComponent},
-  {path: '**', redirectTo: '/records', pathMatch: 'full'}
+  { path: 'records', component: RecordsComponent, canActivate: [AuthGuard] },
+  { path: 'collaborators', component: CollaboratorsComponent, canActivate: [AuthGuard] },
+  { path: 'collaborators/:collaboratorId', component: CollaboratorDetailComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '/records', pathMatch: 'full' }
 ];
 
 @NgModule({
